e453829e0a0c9ba02f8a826fc3fcbd0dc439d7be
Andre the Frog <65509656+andrelabbe@users.noreply.github.com>
2024-01-06 16:13:00 +0000

    Add project files.

---
+++ ToggleSnip/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ToggleSnip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Start the snipping tool process
+            Process snippingTool = new Process();
+            snippingTool.StartInfo.FileName = @"C:\Program Files\WindowsApps\Microsoft.ScreenSketch_11.2310.54.0_x64__8wekyb3d8bbwe\SnippingTool\SnippingTool.exe";
+            snippingTool.Start();
+
+            // Wait for the process to be ready
+            snippingTool.WaitForInputIdle();
+
+            // Start of the eky stroke sequence
+
+            Thread.Sleep(400);
+            SendKeys.SendWait("{TAB}");
+            Thread.Sleep(400);
+            SendKeys.SendWait("{TAB}");
+            Thread.Sleep(400);
+            SendKeys.SendWait("{TAB}");
+            Thread.Sleep(400);
+            SendKeys.SendWait("{TAB}");
+            Thread.Sleep(400);
+            SendKeys.SendWait("{TAB}");
+            Thread.Sleep(400);
+            SendKeys.SendWait("{UP}");
+            Thread.Sleep(400);
+            SendKeys.SendWait("{ENTER}");
+            Thread.Sleep(400);
+            SendKeys.SendWait("{TAB}");
+            Thread.Sleep(400);
+            SendKeys.SendWait("{TAB}");
+            Thread.Sleep(400);
+            SendKeys.SendWait(" ");
+            Thread.Sleep(400);
+            SendKeys.SendWait("% ");
+            Thread.Sleep(400);
+            SendKeys.SendWait("{UP}");
+            Thread.Sleep(400);
+            SendKeys.SendWait("{ENTER}");
+
+
+        }
+    }
+}
